ad8b61dc281d729ab5e5f3e1e8c251eb50decf1f
Joe Timmins <joe@novoda.com>
2017-12-15 13:16:06 +0000

    Remove unused code

--- core/src/main/java/com/novoda/materialised/hackernews/navigator/Navigator.kt
+++
@@ -1,5 +0,0 @@
-package com.novoda.materialised.hackernews.navigator
-
-interface Navigator {
-    fun navigateTo(uri: String) // using String here sucks, but Intent needs an Android Uri, not a Java one :/
-}
